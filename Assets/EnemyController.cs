490e5357a1fe6041a819fc946cc4cff3c9e9a24a
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-03-28 14:37:05 +0100

    Dodano kontroler wroga i zmiany w scenie
    Najważniejsze zmiany w pliku `SampleScene.unity` obejmują:
    - Zmiany w hierarchii obiektów (`m_TransformParent`).
    - Dodanie nowych obiektów (`DefaultPath`, `Enemy`, `WP1`, `WP2`, `WP3`, `Tiles`).
    - Korekty pozycji i rotacji obiektów (`m_LocalPosition`, `m_LocalRotation`).
    - Zmiany w ustawieniach renderowania i oświetlenia (`m_AmbientIntensity`, `m_Color`).
    - Dodanie nowego korzenia sceny (`m_Roots`).
    
    Dodano nową klasę `EnemyController` w pliku `EnemyController.cs`, która zarządza ruchem wrogów:
    - Importy bibliotek.
    - Deklaracja publicznego obiektu `path` i zmiennej `speed`.
    - Prywatna lista `waypoints`.
    - Metody `Start()` i `Update()` do zarządzania ruchem wrogów.
    
    Dodano nowy tag `Enemy` w pliku `TagManager.asset`.
    Dodano plik `EnemyController.cs.meta` z informacjami meta.

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    //path to jest obiekt zawieraj¹ce kolejne waypointy jako obiekty potomne
+    public GameObject path;
+    public float speed = 5.0f;
+    List<Transform> waypoints = new List<Transform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        //iteruj przez kolejne waypointy i dodaj je do listy waypoints
+        for (int i = 0; i < path.transform.childCount; i++)
+        {
+            waypoints.Add(path.transform.GetChild(i).gameObject.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(waypoints.First());
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if(Vector3.Distance(transform.position, waypoints.First().position) < 0.01f)
+        {
+            waypoints.RemoveAt(0);
+        }
+    }
+}
