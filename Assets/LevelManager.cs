4c9ddc2601faa35941edbd3c4b2ae24102406e99
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-04-04 14:50:46 +0200

    Dodano obsługę złota w LevelManager i EnemyController
    Wprowadzono zmiany w pliku `LevelManager.cs`, dodając obsługę tekstu za pomocą `TextMeshProUGUI` do wyświetlania ilości złota. Zmienna `goldText` oraz metoda `AddGold` umożliwiają aktualizację i zwiększanie ilości złota.
    
    Zaktualizowano komponenty w `SampleScene.unity`, zmieniając nazwę obiektu na "GoldCounter" oraz dodano odniesienie do `goldText`.
    
    W pliku `EnemyController.cs` dodano odniesienie do `LevelManager`, co pozwala na dodawanie złota po zderzeniu z obiektem oznaczonym jako "Bullet". Zaktualizowano logikę w metodzie `OnCollisionEnter`, aby zwiększać ilość złota w grze.

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public TextMeshProUGUI goldText;
+    int gold;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gold = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        goldText.text = "Gold: " + gold.ToString();
+    }
+    public void AddGold(int amount)
+    {
+        gold += amount;
     }
 }
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,10 +9,12 @@ public class EnemyController : MonoBehaviour
     //path to jest obiekt zawieraj¹ce kolejne waypointy jako obiekty potomne
     public GameObject path;
     public float speed = 5.0f;
+    LevelManager levelManager;
     List<Transform> waypoints = new List<Transform>();
     // Start is called before the first frame update
     void Start()
     {
+        levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
         //iteruj przez kolejne waypointy i dodaj je do listy waypoints
         for (int i = 0; i < path.transform.childCount; i++)
         {
@@ -23,6 +25,11 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(waypoints.Count == 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
         transform.LookAt(waypoints.First());
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         if(Vector3.Distance(transform.position, waypoints.First().position) < 0.1f)
@@ -32,9 +39,10 @@ public class EnemyController : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision!");
+        //Debug.Log("Collision!");
         if (collision.gameObject.tag == "Bullet")
         {
+            levelManager.AddGold(1);
             Destroy(gameObject);
         }
     }
